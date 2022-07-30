5a8d8d30dac5be5197f380a3236da3d0d1d9aca8
Serjeel-Ranjan-911 <serjeelranjan@gmail.com>
2022-07-30 15:38:19 +0530

    added few songs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-import { alphabets, songs } from "./data";
 import Song from "./Components/song";
 import AlphabetSelector from "./Components/alphabetSelector";
 
